bd6fa977f479ef8162f4789fea6165a887ab4727
Adrian Bieniek <a.bieniek.kontakt@gmail.com>
2023-10-30 01:26:28 +0100

    Add generate user profile id.

---
+++ utils/generateProfileId.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const generateProfileId = async (firstName: string, lastName: string) => {
+	let profileId = `${firstName.toLowerCase()}.${lastName.toLowerCase()}`;
+
+	const idNumber = (
+		await prisma.user.findMany({
+			where: {
+				profileId: {
+					contains: profileId,
+				},
+			},
+		})
+	).length;
+
+	return idNumber > 2 ? (profileId += `.${idNumber + 1}`) : profileId;
+};
+
+export default generateProfileId;
