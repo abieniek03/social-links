8bf373eb89466512529357e05daf598380a8fa83
Adrian Bieniek <a.bieniek.kontakt@gmail.com>
2023-12-20 02:40:11 +0100

    Fixed footer.

--- app/(application)/page.tsx
+++ app/(application)/page.tsx
@@ -5,7 +5,7 @@ import headerImage from "@/assets/images/header.jpg";
 const Home: FC = () => {
 	return (
 		<>
-			<header className="relative my-[8vh] font-bold flex flex-col justify-center items-center gap-3 md:flex-row md:justify-between">
+			<header className="relative my-24 font-bold flex flex-col justify-center items-center gap-3 md:flex-row md:justify-between">
 				<div className="relative mb-6 md:mb-0 before:absolute before:bg-primary before:h-2 before:w-full before:rounded-lg before:-bottom-5  ">
 					<h1 className="mb-1 text-center text-4xl md:text-left md:text-5xl lg:text-6xl">
 						Łatwy i szybki <span className="text-primary">dostęp</span>
@@ -13,16 +13,16 @@ const Home: FC = () => {
 					<p className="text-2xl">do&nbsp;mediów społecznościowych.</p>
 				</div>
 
-				<Image src={headerImage} alt="" width={200} className="rounded-lg w-full max-w-[250px] md:max-w-xs" />
+				<Image src={headerImage} alt="" width={350} className="rounded-lg" />
 			</header>
 
-			<footer className="absolute w-full bottom-0 left-0 text-neutral-300 dark:text-slate-500">
+			<footer className="md:absolute bottom-0 left-0 w-full text-neutral-300 dark:text-slate-500">
 				<div className="max-w-screen-xl mx-auto border-t dark:border-dark-hover py-4 flex flex-col lg:flex-row justify-between items-center">
 					<span>
 						Projekt do portfolio stworzony przez:{" "}
 						<a href="https://www.abieniek.dev/" className="underline">
 							a.bieniek.dev
-						</a>
+						</a>{" "}
 					</span>
 					<span>Listopad 2023</span>
 				</div>
